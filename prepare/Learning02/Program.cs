92f7ade0522431c3741f3fbd37b33c93f5afd861
ChaseB-creator <Chasioboxio@gmail.com>
2025-10-01 14:47:17 -0600

    Learning02 Completion

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 world!");
+
+        // Job 1 Creation (branching from Job.cs):
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Job 2 Creation (branching from Job.cs): 
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Adding jobs to Resume (branching from Resume.cs):
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Displaying stored jobs from Resume (branching from Resume.cs):
+        myResume.Display();
     }
 }
