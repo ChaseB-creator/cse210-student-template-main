d649b5395e8e6ff8d310c64c5fd1da235cada9a6
ChaseB-creator <Chasioboxio@gmail.com>
2025-10-16 11:12:07 -0600

    Learning03 submission corrections made

--- prepare/Learning03/Fractions.cs
+++ prepare/Learning03/Fractions.cs
@@ -5,51 +5,42 @@ public class Fraction {
     private int _bottom;
 
 
-    public Fraction()
-    {
+    public Fraction() {
         _top = 1;
         _bottom = 1;
     }
 
-    public Fraction(int top)
-    {
+    public Fraction(int top) {
         _top = top;
         _bottom = 1;
     }
 
-    public Fraction(int top, int bottom)
-    {
+    public Fraction(int top, int bottom) {
         _top = top;
         _bottom = bottom;
     }
 
-    public int GetTop()
-    {
+    public int GetTop() {
         return _top;
     }
 
-    public void setTop(int top)
-    {
+    public void setTop(int top) {
         _top = top;
     }
 
-    public int GetBottom()
-    {
+    public int GetBottom() {
         return _bottom;
     }
 
-    public void setBottom(int bottom)
-    {
+    public void setBottom(int bottom) {
         _bottom = bottom;
     }
 
-    public string GetFractionString()
-    {
+    public string GetFractionString() {
         return $"{_top}/{_bottom}";
     }
     
-    public double GetDecimalValue()
-    {
+    public double GetDecimalValue() {
         return (double)_top / (double)_bottom;
     }
 
