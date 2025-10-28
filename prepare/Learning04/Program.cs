376335f8564d962de5aef6f83f27f3d88b5e8006
ChaseB-creator <Chasioboxio@gmail.com>
2025-10-28 12:00:01 -0600

    W07 Learning Activity Submission

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+
+        Assignments assignment = new Assignments("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "European History", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        writingAssignment writingAssignment = new writingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,18 @@
+public class writingAssignment : Assignments
+{
+    private string _title;
+
+    public writingAssignment(string studentName, string topic, string title)
+
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+    
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+        return $"{_title} by {studentName}";
+    }
+
+}
