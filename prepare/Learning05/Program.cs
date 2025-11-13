6bf875359bf3ea3728aceaa893191e639fe57ffc
ChaseB-creator <Chasioboxio@gmail.com>
2025-11-13 16:09:22 -0700

    Le

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+    
+        Square s = new Square("Red", 5);
+        Console.WriteLine($"Square Color: {s.GetColor()}");
+        Console.WriteLine($"Square Area: {s.GetArea()}");
+
+   
+        Rectangle r = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle Color: {r.GetColor()}");
+        Console.WriteLine($"Rectangle Area: {r.GetArea()}");
+
+        Circle c = new Circle("Green", 3);
+        Console.WriteLine($"Circle Color: {c.GetColor()}");
+        Console.WriteLine($"Circle Area: {c.GetArea()}");
+
+    
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        Console.WriteLine("\n--- Shape List ---");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} | Area: {shape.GetArea()}");
+        }
     }
 }
